accc17da7deb6501367e1a04d9ba3dd4a14920b0
Ilona Bloem <im.bloem89@gmail.com>
2026-06-10 11:36:38 -0400

    add new figures

--- Code/createFigures.m
+++ Code/createFigures.m
@@ -16,10 +16,28 @@ allResults      = loadModelResults(opts);
 % Figure 2 - comparison deconvolution vs model-based
 createModelSchematic
 
-% Figure 4 - CRF parameter comparison between model-based and deconvolution approaches
-doStats         = true;
-fig_CompareModelParams(opts, allResults(1), doStats);
+% Figure 3 - model schematic
+visualizeNakaRushtonParameters(opts)
 
-% Figure 5 - CRF parameters for continuous contrast presentation experimental design
+% Figure 4 - CRF parameter comparison between model-based and deconvolution approaches
+[fisherCorr, avgParams]  = fig_CompareModelParams(opts, allResults(1));
+
+% Do group-level stats for modeling comparisons on same dataset, 
+% provides ttests on avg parameters and fisher z correlations
+doStats         = true;
+if doStats > 0
+    computeStats_modelComparison(opts, avgParams, fisherCorr)
+end
+
+% Figure 5 - full CRF shapes are comparible between models
+fig_shapeComparison(opts, allResults(1))
+
+% Figure 6 -median parameter estimates and voxel-wise eccen vs C50
+avgParams   = fig_CompareModelParams(opts, allResults(1));
+
+
+
+
+% Figure 6 - CRF parameters for continuous contrast presentation experimental design
 doStats         = true;
 fig_ContinuousResults(opts, allResults, doStats);
