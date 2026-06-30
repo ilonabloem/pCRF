c623afddfd0efcabd76e5e491ee3bc2d245601ee
Ilona Bloem <im.bloem89@gmail.com>
2026-06-30 16:18:57 +0200

    Add new figures for manuscript

--- Code/createFigures.m
+++ Code/createFigures.m
@@ -20,24 +20,31 @@ createModelSchematic
 visualizeNakaRushtonParameters(opts)
 
 % Figure 4 - CRF parameter comparison between model-based and deconvolution approaches
-[fisherCorr, avgParams]  = fig_CompareModelParams(opts, allResults(1));
+fig_CompareModelParams(opts, allResults(1));
 
 % Do group-level stats for modeling comparisons on same dataset, 
 % provides ttests on avg parameters and fisher z correlations
 doStats         = true;
 if doStats > 0
+    avgParams.modelbasedParams  = allResults(1).modelbasedParams;
+    avgParams.deconvParams      = allResults(1).deconvParams;
+    avgParams.paramLabels       = {'Rmax', 'Slope', 'C50'}; % order of avg params 
+    fisherCorr                  = allResults(1).fisherCorr;
     computeStats_modelComparison(opts, avgParams, fisherCorr)
 end
 
 % Figure 5 - full CRF shapes are comparible between models
 fig_shapeComparison(opts, allResults(1))
 
-% Figure 6 -median parameter estimates and voxel-wise eccen vs C50
-avgParams   = fig_CompareModelParams(opts, allResults(1));
-
-
-
-
 % Figure 6 - CRF parameters for continuous contrast presentation experimental design
 doStats         = true;
 fig_ContinuousResults(opts, allResults, doStats);
+
+% Figure 7 - voxel-wise patterns, C50 increases as function of eccentricity
+fig_voxelwisePatterns(opts, allResults, doStats)
+
+% supplementary figure illustrating voxel selection thresholds
+suppfig_voxelSelection(opts, allResults)
+
+% supplementary figure illustrating HRF params
+suppfig_avgHRFparams(opts, allResults)
